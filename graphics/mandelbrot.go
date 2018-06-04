274cc38c37f5c6b8ef2c5ab138d792ef15ed8f8c
ilmikko <mikko@5x.fi>
2018-06-04 12:19:16 +0100

    Mandelbrot now evaluates simple math expressions

--- graphics/mandelbrot.go
+++ graphics/mandelbrot.go
@@ -4,15 +4,44 @@ import "github.com/fogleman/gg";
 import "fmt";
 
 func main(){
-	fmt.Printf("%s\n","test");
+	fmt.Printf("%s\n","Running {{EXPRESSION}}");
 
-	w := 100;
-	h := 100;
+	// Canvas definitions
+	pixelW := 100;
+	pixelH := 100;
 
-	ctx := gg.NewContext(w, h);
+	// Math definitions
+	mathX := 0;
+	mathY := 0;
+	mathW := 4;
+	mathH := 4;
+
+	ctx := gg.NewContext(pixelW, pixelH);
+
+	ctx.SetRGB(1,1,1);
+	ctx.Clear();
 
 	ctx.SetRGB(0,0,0);
-	ctx.SetPixel(w/2,h/2);
+	// Loop through every pixel
+	// TODO: This loop could be threaded, as a pixel need not know its neighbor to calculate its value.
+	for pixelX := 0; pixelX < pixelW; pixelX++ {
+		for pixelY := 0; pixelY < pixelH; pixelY++ {
+			// Convert the pixelX and pixelY to x and y in the definition.
+			// We know that x and y are the center coordinates, w and h are the width and height of our "math viewport".
+			// This means that the left side of the canvas, where pixelX = 0, will be mathX-mathW/2.
+			// The right side where pixelX = pixelW is mathX+mathW/2.
 
-	ctx.SavePNG("mandelbrot.png");
+			px := float64(pixelX)/float64(pixelW); // 0..1
+			py := float64(pixelY)/float64(pixelH); // 0..1
+
+			x := float64(mathX) - float64(mathW)/2 + px*float64(mathW);
+			y := float64(mathY) - float64(mathH)/2 + py*float64(mathH);
+
+			if {{EXPRESSION}} {
+				ctx.SetPixel(pixelX,pixelY);
+			}
+		}
+	}
+
+	ctx.SavePNG("{{EXPRESSION}}.png");
 }
